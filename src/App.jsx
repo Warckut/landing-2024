219697a7969575a8cdbc943be6aade04536662e7
Warckut <warckut@gmail.com>
2024-07-08 13:33:29 +0500

    fix: роуты перенесены в массив объектов

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 
+const routes = [
+  { path: '/', element: Home },
+  { path: '/signIn', element: SignIn },
+  { path: '/signUp', element: SignUp },
+];
+
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/signIn' element={<SignIn />} />
-      <Route path='/signUp' element={<SignUp />} />
+      {routes.map((props) => (
+        <Route {...props} />
+      ))}
     </Routes>
   );
 }
